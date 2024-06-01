17c1042324cafd2bd4553544fcc0e4160ee8ddc3
Chromax-design <chromax.designs@gmail.com>
2024-06-01 22:28:52 +0100

    added sentry

--- components/ui/CardEffect.tsx
+++ components/ui/CardEffect.tsx
@@ -12,69 +12,85 @@ export const HoverEffect = ({ items, className }: any) => {
     <div
       className={cn("grid grid-cols-1 md:grid-cols-2 py-10 gap-4", className)}
     >
-      {items.map(({ id, title, des, img, iconLists, link }: any, idx: any) => (
-        <Link
-          href={link}
-          key={id}
-          className="relative group block p-2 h-full w-full"
-          onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence>
-          <Card>
-            <div
-              className="relative flex items-center justify-center mb-10"
-              
-            >
-              <div className="relative h-full w-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                <img src="/bg.png" alt="background image" />
+      {items.map(
+        (
+          {
+            id,
+            title,
+            des,
+            img,
+            iconLists,
+            link,
+          }: {
+            id?: number;
+            title?: string;
+            des?: string | React.ReactNode;
+            img?: string;
+            iconLists: any;
+            link: string;
+          },
+          idx: any
+        ) => (
+          <Link
+            href={link}
+            key={id}
+            className="relative group block p-2 h-full w-full"
+            onMouseEnter={() => setHoveredIndex(idx)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <AnimatePresence>
+              {hoveredIndex === idx && (
+                <motion.span
+                  className="absolute inset-0 h-full w-full bg-slate-800/[0.8] block rounded-3xl"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <Card>
+              <div className="relative flex items-center justify-center mb-10">
+                <div className="relative h-full w-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="background image" />
+                </div>
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
-              <img src={img} alt={title} className="z-10 absolute bottom-0" />
-            </div>
-            <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-              {title}
-            </h1>
-            <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-              {des}
-            </p>
-            <div className="flex items-center justify-between mt-7 mb-3">
-              <div className="flex items-center">
-                {iconLists.map((icon: any, index: any) => (
-                  <div
-                    key={icon}
-                    className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                    style={{ transform: `translateX(-${5 * index * 2}px)` }}
-                  >
-                    <img src={icon} alt="icon" className="p-2" />
-                  </div>
-                ))}
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {iconLists.map((icon: any, index: any) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                    >
+                      <img src={icon} alt="icon" className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-lg md:text-xs text-sm">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#cbacf9" />
+                </div>
               </div>
-              <div className="flex justify-center items-center">
-                <p className="flex lg:text-lg md:text-xs text-sm">
-                  Check Live Site
-                </p>
-                <FaLocationArrow className="ms-3" color="#cbacf9" />
-              </div>
-            </div>
-          </Card>
-        </Link>
-      ))}
+            </Card>
+          </Link>
+        )
+      )}
     </div>
   );
 };
