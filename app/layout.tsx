f62f40cbf6e60d60d4e82eb165ba2072f295660c
Chromax-design <chromax.designs@gmail.com>
2024-06-02 16:03:34 +0100

    added a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
   title: "Chromax Dev portfolio",
   description:
     "Explore the portfolio of Godskey, a skilled web developer specializing in creating responsive, user-friendly websites. Discover innovative projects showcasing expertise in HTML, CSS, JavaScript, and modern frameworks. Transform your online presence with professional web development services",
+    icons:{
+      icon: '/favicon.png'
+    }
 };
 
 export default function RootLayout({
