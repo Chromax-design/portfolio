d6d84fe155997f95c59d06f2a17ebba3f5b283ef
Chromax-design <chromax.designs@gmail.com>
2024-08-01 12:15:56 +0100

    made about page responsive

--- src/components/About/Insight.tsx
+++ src/components/About/Insight.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Insight = ({ insights }: { insights: string[] }) => {
   return (
-    <div className="w-full bg-accent text-light whitespace-nowrap overflow-hidden">
-      <div className="animate-roll w-full py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-base">
+    <div className="w-full bg-accent dark:bg-accentDark dark:text-dark text-light whitespace-nowrap overflow-hidden">
+      <div className="animate-roll w-full py-5 sm:py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-sm sm:text-base">
         {insights.map((insight) => (
           <div key={insight}>
             {insight}
--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -16,14 +16,14 @@ const SkillList: string[] = [
 
 const Skills = () => {
   return (
-    <section className="w-full flex flex-col p-20 border-b-2 border-solid border-dark">
-      <span className="font-semibold text-4xl text-accent">
+    <section className="w-full flex flex-col p-5 xs:p-10 sm:p-12 md:p-16 lg:p-20 border-b-2 border-solid border-dark dark:border-light">
+      <span className="font-semibold text-lg sm:text-3xl md:text-4xl text-accent dark:text-accentDark">
         I&apos; comfortable in...
       </span>
-      <ul className="flex flex-wrap mt-8 justify-start gap-6">
+      <ul className="flex flex-wrap mt-8 justify-center xs:justify-start gap-3 xs:gap-4 md:gap-6">
         {SkillList.map((item) => (
           <li
-            className="font-semibold inline-block capitalize text-2xl py-5 px-12 border-2 border-solid border-dark rounded hover:scale-105 transition-all ease duration-200 cursor-pointer"
+            className="font-semibold dark:font-normal inline-block capitalize text-base xs:text-lg sm:text-xl md:text-2xl py-2 sm:py-4 lg:py-5 px-4 xs:px-6 sm:px-8 lg:px-12 border-2 border-solid border-dark dark:border-light rounded hover:scale-105 transition-all ease duration-200 cursor-pointer"
             key={item}
           >
             {item}
