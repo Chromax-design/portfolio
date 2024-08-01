f85b3e93b034ed60bf7343572b5fdc68a80533e7
Chromax-design <chromax.designs@gmail.com>
2024-08-01 12:32:26 +0100

    made contact page responsive

--- src/app/(info)/contact/page.tsx
+++ src/app/(info)/contact/page.tsx
@@ -11,12 +11,12 @@ export const metadata: Metadata = {
 
 const ContactPage = () => {
   return (
-    <section className="w-full h-[75vh] border-b-2 border-solid border-dark flex flex-row items-center justify-center text-dark">
-      <div className="inline-block w-2/5 h-full border-solid border-r-2 border-b-dark">
+    <section className="w-full h-auto md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
+      <div className="inline-block w-full sm:w-4/5 md:w-2/5 h-full border-solid md:border-r-2 border-b-dark dark:border-b-light">
         <LottieAnimation />
       </div>
-      <div className="w-3/5 flex flex-col items-start justify-center px-10 pb-8">
-        <h2 className="font-bold capitalize text-4xl">Let&apos;s connect</h2>
+      <div className="w-full md:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 pb-8">
+        <h2 className="font-bold capitalize text-2xl xs:text-3xl sm:text-4xl">Let&apos;s connect</h2>
         <ContactForm />
       </div>
     </section>
